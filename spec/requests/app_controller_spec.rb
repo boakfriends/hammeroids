430ca91eb6cdcf7c29130d88ef25e4f925b5a487
Jason Porter <jason.porter@netcentric.biz>
2019-02-14 12:19:48 +0100

    Added test for /join page

--- spec/requests/app_controller_spec.rb
+++ spec/requests/app_controller_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe "Hammeroids::App", type: :request do
       expect(last_response.successful?).to eql true
     end
   end
+
+  describe 'GET /join' do
+    let(:path) {'/join'}
+
+    it 'is successful' do
+      get path
+      expect(last_response.successful?).to eql true
+    end
+  end
 end
