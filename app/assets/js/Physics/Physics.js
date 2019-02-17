ddc3b0a1495bdd0b2e8b4f4559ca3da8606b6bad
Jason Porter <jason.porter@netcentric.biz>
2019-02-17 21:36:01 +0100

    Cosmetic changes only - changed indentation to 2 spaces fos JS

--- app/assets/js/Physics/Physics.js
+++ app/assets/js/Physics/Physics.js
@@ -1,54 +1,54 @@
 var Physics = function(friction, acceleration, startYPos, startXPos){
-	var xMomentum = 0,
-		yMomentum = 0,
-		angularMomentum = 0,
-		yPos = startYPos,
-		xPos = startXPos,
-		angle = 0;
+  var xMomentum = 0,
+    yMomentum = 0,
+    angularMomentum = 0,
+    yPos = startYPos,
+    xPos = startXPos,
+    angle = 0;
 
-	function accel(){
-		var yMomentumAddition = -Math.cos(angle * Math.PI / 180),
-			xMomentumAddition = -Math.cos((angle + 90) * Math.PI / 180);
-		yMomentum = yMomentum + yMomentumAddition;
-		xMomentum = xMomentum + xMomentumAddition;
-	}
+  function accel(){
+    var yMomentumAddition = -Math.cos(angle * Math.PI / 180),
+      xMomentumAddition = -Math.cos((angle + 90) * Math.PI / 180);
+    yMomentum = yMomentum + yMomentumAddition;
+    xMomentum = xMomentum + xMomentumAddition;
+  }
 
-	function update(){
-		yMomentum = yMomentum * friction;
-		xMomentum = xMomentum * friction;
-		yPos = yPos + yMomentum;
-		xPos = xPos + xMomentum;
-		angle = angularMomentum + angle % 360;
-		angularMomentum = angularMomentum * friction;
-	}
+  function update(){
+    yMomentum = yMomentum * friction;
+    xMomentum = xMomentum * friction;
+    yPos = yPos + yMomentum;
+    xPos = xPos + xMomentum;
+    angle = angularMomentum + angle % 360;
+    angularMomentum = angularMomentum * friction;
+  }
 
-	function getState(){
-		return {
-			x: xPos,
-			y: yPos,
-			angle: angle
-		}
-	}
+  function getState(){
+    return {
+      x: xPos,
+      y: yPos,
+      angle: angle
+    };
+  }
 
-	function turn(momentumFunction){
-		angularMomentum = momentumFunction(angularMomentum, acceleration);
-	}
+  function turn(momentumFunction){
+    angularMomentum = momentumFunction(angularMomentum, acceleration);
+  }
 
-	function getTransform(x, y){
-		var xPoint = x * Math.cos(angle * Math.PI / 180) - y * Math.sin(angle * Math.PI / 180) + xPos,
-			yPoint = x * Math.sin(angle * Math.PI / 180) + y * Math.cos(angle * Math.PI / 180) + yPos;
-		return {
-			x: xPoint,
-			y: yPoint
-		};
-	}
+  function getTransform(x, y){
+    var xPoint = x * Math.cos(angle * Math.PI / 180) - y * Math.sin(angle * Math.PI / 180) + xPos,
+      yPoint = x * Math.sin(angle * Math.PI / 180) + y * Math.cos(angle * Math.PI / 180) + yPos;
+    return {
+      x: xPoint,
+      y: yPoint
+    };
+  }
 
-	return {
-		update: update,
-		accel: accel,
-		turn: turn,
-		getTransform: getTransform,
-		getState: getState
-	};
+  return {
+    update: update,
+    accel: accel,
+    turn: turn,
+    getTransform: getTransform,
+    getState: getState
+  };
 
 };
--- app/assets/js/Sockets.js
+++ app/assets/js/Sockets.js
@@ -1,25 +1,25 @@
 var Sockets = function(){
-	var scheme = "ws://",
-		uri = scheme + window.document.location.host + ':8080' + '/game',
-		ws = new WebSocket("ws://localhost:8080/game"),
-		networkObjects = [];
+  var scheme = "ws://",
+    uri = scheme + window.document.location.host + ':8080' + '/game',
+    ws = new WebSocket("ws://localhost:8080/game"),
+    networkObjects = [];
 
-	function updatePlayerShipState(playerShipState){
-		ws.send(JSON.stringify(playerShipState));
-	}
+  function updatePlayerShipState(playerShipState){
+    ws.send(JSON.stringify(playerShipState));
+  }
 
-	function getNetworkObjects(){
-		return networkObjects;
-	}
+  function getNetworkObjects(){
+    return networkObjects;
+  }
 
-	ws.onmessage = function(event){
-		var data = JSON.parse(event.data);
+  ws.onmessage = function(event){
+    var data = JSON.parse(event.data);
                 console.log(data);
 
-	};
+  };
 
-	return {
-		updatePlayerShipState: updatePlayerShipState,
-		getNetworkObjects: getNetworkObjects
-	};
+  return {
+    updatePlayerShipState: updatePlayerShipState,
+    getNetworkObjects: getNetworkObjects
+  };
 };
