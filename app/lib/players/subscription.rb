4b7fd11da24086e4f5e02a0e95999e7dca7f609b
Dan Goodwin <dangoodwin1@gmail.com>
2019-03-10 12:17:22 +0000

    Move Subscription welcome into Event class.
    Moves the Welcome JSON out of the Subscription object and into it's own
    class. Adds support for multiple subscription events should we need them
    later.

--- app/lib/players/subscription.rb
+++ app/lib/players/subscription.rb
@@ -2,17 +2,25 @@ module Hammeroids
   module Players
     # Subscribes @connection to @channel and defines onmessage and onclose events.
     class Subscription
+      EVENTS = [
+        Hammeroids::Subscriptions::Events::Welcome
+      ].freeze
+
       def initialize(connection, channel)
         @connection = connection
         @channel = channel
       end
 
       def create
-        @connection.send(%({"type":"welcome", "id": #{id}}))
+        create_events
         id
       end
 
       private
+
+      def create_events
+        EVENTS.each { |event| event.new(@connection, id).create }
+      end
 
       def id
         @id ||= @channel.subscribe { |message| @connection.send(message) }
